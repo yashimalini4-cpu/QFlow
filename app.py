763b2629a1a6d6b49caeb0761bf7bffe5c98e461
Yashi Malini <yashimalini4@gmail.com>
2026-07-12 21:04:03 +0530

    Fix Render port binding

--- app.py
+++ app.py
@@ -671,5 +671,8 @@ def completed_tokens():
 
 # ---------------- RUN APP ---------------- #
 
+import os
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=False)
